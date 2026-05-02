9892be3e1c621d8b27ce8135a71cfca28f52677f
omardhman3-eng <omardhman3@gmail.com>
2026-05-02 15:51:42 +0300

    Update midas_login.py

--- midas_login.py
+++ midas_login.py
@@ -15,8 +15,8 @@ def setup_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.binary_location = "/usr/bin/chromium"
-    
+    chrome_options.binary_location = "/snap/bin/chromium"
+
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
